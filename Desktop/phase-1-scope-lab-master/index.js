d1ebed0daf7ba5d7dab85894230958e9f402e6e5
tomiece317 <ceyda666esen@gmail.com>
2023-03-23 18:39:07 +0300

    Scoped

---
+++ Desktop/phase-1-scope-lab-master/index.js
@@ -0,0 +1,15 @@
+// Write your solution in this file!
+var customerName = "jada";
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+  bestCustomer = "jada";
+}
+const leastFavoriteCustomer = "jada";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "mike";
+}
