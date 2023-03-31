15bb68a23c22f9d166decfc499c983987dbd0870
tomiece317 <ceyda666esen@gmail.com>
2023-04-01 01:06:57 +0300

    done

---
+++ Desktop/phase-1-arithmetic-lab-master/index.js
@@ -0,0 +1,15 @@
+
+// Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 16;
+let num4 = 6;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+
+let setOfNumbers = [5, 10, 15, 20];
+let max = Math.max(...setOfNumbers);
