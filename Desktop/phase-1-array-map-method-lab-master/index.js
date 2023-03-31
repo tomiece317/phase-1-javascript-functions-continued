718fb6f04a91e8b7e6572035f3f6e584262602e8
tomiece317 <ceyda666esen@gmail.com>
2023-04-01 00:01:18 +0300

    done

---
+++ Desktop/phase-1-array-map-method-lab-master/index.js
@@ -0,0 +1,20 @@
+const tutorials = [
+  'what does the this keyword mean?',
+  'What is the Constructor OO pattern?',
+  'implementing Blockchain Web API',
+  'The Test Driven Development Workflow',
+  'What is NaN and how Can we Check for it',
+  'What is the difference between stopPropagation and preventDefault?',
+  'Immutable State and Pure Functions',
+  'what is the difference between == and ===?',
+  'what is the difference between event capturing and bubbling?',
+  'what is JSONP?'
+];
+
+const titleCased = () => {
+  return tutorials.map(function(title){
+    return title.split(' ').map(function(word){
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' ');
+  });
+}
